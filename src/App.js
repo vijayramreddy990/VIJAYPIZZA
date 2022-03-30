bbc8241375a51869d385651523610bae89957ed2
vijay <vijayramreddy990@gmail.com>
2022-03-30 22:34:14 +0530

    navbarUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>SHEY PIZZA</h1>
+      <Navbar />
     </div>
   );
 }
